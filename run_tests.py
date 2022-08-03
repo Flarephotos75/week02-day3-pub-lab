2e11da6f612fbfb47f5bedb30a277f3b0f640d84
Stephen Kinnaird <slkinnaird@gmail.com>
2022-08-03 14:58:39 +0100

    Customer tests completed

--- run_tests.py
+++ run_tests.py
@@ -1,6 +1,7 @@
 import unittest
 from tests.pub_test import TestPub
 from tests.test_drinks import TestDrinks
+from tests.customer_test import TestCustomer
 
 if __name__ == '__main__':
     unittest.main()
---
+++ tests/customer_test.py
@@ -0,0 +1,21 @@
+import unittest
+from classes.customer import Customer
+
+class TestCustomer(unittest.TestCase):
+    
+    def setUp(self):
+        self.stephen = Customer("Stephen", 20, 30, 0)
+        self.john = Customer("John", 40, 25, 0)
+        self.adam = Customer("Adam", 10, 17, 0)
+
+    def test_get_customer_name(self):
+        self.assertEqual("Stephen", self.stephen.name)
+
+    def test_get_customer_wallet(self):
+        self.assertEqual(40, self.john.wallet)
+
+    def test_get_customer_age(self):
+        self.assertEqual(17, self.adam.age)
+
+    def test_get_drunken_level(self): 
+        self.assertEqual(0, self.stephen.drunken_level)
